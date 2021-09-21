376fd244d94a9b02d0ef1563833e72fe3bc2dbd6
Kevin Mirafuentes <kevin.mirafuentes@gmail.com>
2021-09-21 14:08:13 +0800

    Updated readme file and removed uncessary files

--- resources/js/Pages/Dashboard.js
+++ resources/js/Pages/Dashboard.js
@@ -40,7 +40,7 @@ export default function Dashboard(props) {
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6">
                         <h2>Territories</h2>
-                        <p>Here are the list of territories</p>
+                        <p>Here are the list of territories:</p>
 
                         <ul id="myUL">
                             {Object.keys(territories).map(id => <li key={id} className="pl-4">{createTree(territories[id])}</li>)}
